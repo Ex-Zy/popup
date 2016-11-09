2faae5b5aa7d05720932193d4e0030be0ba760a6
admin\Admin <serega1905@ukr.net>
2016-11-09 02:59:52 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ $(function() {
 		allContent = $(document).find('.js-content-item'), 
 		activeLink = allLinks.filter('.is-active').length ? allLinks.filter('.is-active') : allLinks.eq(0),
 		linkData = activeLink.data('link'),
-		filtered = filterElements(linkData);
+		filtered = filterElements(linkData),
+		slider1 = $(document).find(".js-slider-index1"),
+		slider2 = $(document).find(".js-slider-index2");
 
 	function filterElements(linkData) {
 		var link, content;
@@ -30,7 +32,25 @@ $(function() {
 			filtered = filterElements(linkData);
 
 		e.preventDefault();
-		addClassElement(filtered.link, filtered.content);	
+		addClassElement(filtered.link, filtered.content);
+		slider1.slick('reinit');
+		slider2.slick('reinit');
 	});
 
-})
+    function initSLider(slider) {
+		slider.on("init", function(){
+	        setTimeout(function(){
+	           slider.addClass("is-ready");
+	        },200);
+	    });
+
+	    slider.not('.slick-initialized').slick({
+	    	slidesToShow: 2,
+	    	slidesToScroll: 1
+	    });
+    }
+
+    initSLider(slider1);
+    initSLider(slider2);
+
+});
