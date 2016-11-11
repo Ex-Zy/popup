e77bf4fc586c26cdfa3f61d8950803efeb42fffc
Ex-Zy <serega1905@ukr.net>
2016-11-11 22:41:19 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 $(function() {
 	let allLinks = $(document).find('.js-tabs-link'); 
 	let allContent = $(document).find('.js-content-item');
-	const ACTIVE_LINK_FOUND = allLinks.filter('.is-active').length;
-	let activeLink = ACTIVE_LINK_FOUND ? allLinks.filter('.is-active') : allLinks.eq(0);
+	let linkFilter = allLinks.filter('.is-active');
+	let activeLink = linkFilter.length ? linkFilter : allLinks.eq(0);
 	let linkData = activeLink.data('link');
 	let filtered = filterElements(linkData);
+	let btn = $('.js-open-popup');
+	let popup = $('.js-popup');
 
 	function filterElements(linkData) {
 		let link, content;
@@ -18,16 +20,13 @@ $(function() {
 		};
 	}
 
-	function initSLider(slider, option) {
-		function visibility(slider) {
-			slider.on('init', function() {
-				setTimeout(function(){
-					slider.addClass("is-ready");
-				},200);
-			});
-		}
-		visibility(slider);
-		slider.not('.slick-initialized').slick(option);
+	function initSlider(slider, option) {
+		slider.on('init', function() {
+			setTimeout(function(){
+				slider.addClass('is-ready');
+			},200);
+		});
+		slider.slick(option);
 	}
 
 	function showActiveElements(currentLink, currentContent) {
@@ -35,7 +34,7 @@ $(function() {
 		
 			allLinks.add(allContent).removeClass('is-active');
 			currentLink.add(currentContent).addClass('is-active');
-			initSLider(currentSlider, {slidesToShow: 2, slidesToScroll: 1});
+			initSlider(currentSlider, {slidesToShow: 2});
 	}
 	
 	showActiveElements(filtered.link, filtered.content);
@@ -49,24 +48,27 @@ $(function() {
 		showActiveElements(filtered.link, filtered.content);
 	});
 
-	$(".js-open-popup").click(function() {
+	btn.click(function() {
 		let that = $(this);
-		let popupActive = $(".js-popup").has('.is-active');
+		let slide = that.parents('.slider-slide');
+		let _slider = that.parents('.slider');
+		let index = +slide.attr('data-slick-index') + 1;
 
-		if(popupActive) {
-			$(".js-popup").addClass('is-active');
-		} else {
-			$(".js-popup").removeClass('is-active');
-		}
+
+		popup.addClass('is-active');
+
+		$('.window').prepend(
+			'<div class="js-slider-popup"></div>'
+		);
 	})
 
 	$('.js-close').click(function() {
-		$(".js-popup").removeClass('is-active');
+		popup.removeClass('is-active');
 	})
 
-	$(document).click(function(e) {
-		if ($(".js-popup").is(e.target) ) {
-			$(".js-popup").removeClass('is-active');
+	popup.click(function(e) {
+		if ($(this).is(e.target) ) {
+			popup.removeClass('is-active');
 		}
 	});
 
